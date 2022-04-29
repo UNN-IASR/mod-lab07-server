6374385b90157600b66b37276f11264f3219d805
Nelly Nikitina <worldpiece000@gmail.com>
2022-04-29 19:18:43 +0300

    new

---
+++ clientserver/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clientserver7
+{
+    internal class Client
+    {
+        private Server server;
+        public Client(Server server)
+        {
+            this.server = server;
+            request += server.proc;
+        }
+
+        public void proc(int num)
+        {
+            procEventArgs args = new procEventArgs();
+
+            args.id = num;
+
+            if (request != null)
+            {
+                request(this, args);
+            }
+        }
+
+        public event EventHandler<procEventArgs> request;
+    }
+}
