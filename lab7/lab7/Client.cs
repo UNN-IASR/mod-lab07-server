583599f3dadec335fafa55f45ac4d3f6441ad83a
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-06-10 16:54:46 +0300

    Add files via upload

---
+++ lab7/lab7/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab7
+{
+    internal class Client
+    {
+        private Server server;
+        public Client(Server server)
+        {
+            this.server = server;
+            request += server.proc;
+        }
+
+        public void proc(int num)
+        {
+            procEventArgs args = new procEventArgs();
+
+            args.id = num;
+
+            if (request != null)
+            {
+                request(this, args);
+            }
+        }
+
+        public event EventHandler<procEventArgs> request;
+    }
+}
+
