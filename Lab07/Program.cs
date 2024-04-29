8ba470a187fb7c5425e947ead54bea00960a05d0
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-04-29 13:37:17 +0300

    should fix koeffs

--- Lab07/Program.cs
+++ Lab07/Program.cs
@@ -1,20 +1,43 @@
 ﻿using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 namespace TPProj{
     class Program
     {
+        static int factorial(int n){
+            if (n == 0)
+                return 1;
+            return n*factorial(n-1);
+        }
         static void Main()
         {
-            Server server = new Server();
+            double applications_intensity = 0.5;
+            double service_intensity = 0.5;
+            int working_time = 10;
+            int capacity = 5;
+            Server server = new Server(service_intensity, working_time, capacity);
             Client client = new Client(server);
             for (int id = 1; id <= 100; id++)
             {
                 client.send(id);
-                Thread.Sleep(50);
+                Thread.Sleep(Convert.ToInt32(Math.Round(working_time/applications_intensity)));
             }
             Console.WriteLine("Всего заявок: {0}", server.requestCount);
             Console.WriteLine("Обработано заявок: {0}", server.processedCount);
             Console.WriteLine("Отклонено заявок: {0}", server.rejectedCount);
+            Console.WriteLine("Временной шаг: {0}", working_time);
+            Console.WriteLine("Интенсивность потока заявок: {0}", applications_intensity);
+            Console.WriteLine("Интенсивность потока обслуживаний: {0}", service_intensity);
+            double some_sum = 0;
+            for (int i = 1; i < capacity; i++)
+                some_sum += Math.Pow(applications_intensity / service_intensity, i) / factorial(i);
+            Console.WriteLine("Вероятность простоя системы: {0}", Math.Round(1 / some_sum, 4));
+            double Pn = Math.Pow(applications_intensity / service_intensity, capacity) / factorial(capacity) / some_sum;
+            Console.WriteLine("Вероятность отказа системы: {0}", Math.Round(Pn, 4));
+            Console.WriteLine("Относительная пропускная способность: {0}", Math.Round(1 - Pn, 4));
+            Console.WriteLine("Абсолютная пропускная способность: {0}", Math.Round(applications_intensity * (1 - Pn), 4));
+            Console.WriteLine("Среднее число занятых каналов: {0}", Math.Round(applications_intensity * (1 - Pn) / service_intensity, 4));
         }
     }
     struct PoolRecord
@@ -26,6 +49,9 @@ namespace TPProj{
     {
         private PoolRecord[] pool;
         private object threadLock = new object();
+
+        private int working_time = 0;
+        private double service_intensity = 0.0;
         public int requestCount = 0;
         public int processedCount = 0;
         public int rejectedCount = 0;
@@ -33,18 +59,26 @@ namespace TPProj{
         {
             pool = new PoolRecord[5];
         }
+
+        public Server(double service_intensity, int working_time=500, int capacity=5){
+            pool = new PoolRecord[capacity];
+            this.service_intensity = service_intensity;
+            this.working_time = working_time;
+        }
         public void proc(object sender, procEventArgs e)
         {
             lock (threadLock)
             {
-                Console.WriteLine("Заявка с номером: {0}", e.id);
+                //Console.WriteLine("Заявка с номером: {0}", e.id);
                 requestCount++;
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < pool.Length; i++)
                 {
                     if (!pool[i].in_use)
                     {
                         pool[i].in_use = true;
+#pragma warning disable CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
                         pool[i].thread = new Thread(new ParameterizedThreadStart(Answer));
+#pragma warning restore CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
                         pool[i].thread.Start(e.id);
                         processedCount++;
                         return;
@@ -58,11 +92,11 @@ namespace TPProj{
             int id = (int)arg;
             //for (int i = 1; i < 9; i++)
             //{
-            Console.WriteLine("Обработка заявки: {0}", id);
-            //Console.WriteLine("{0}",Thread.CurrentThread.Name);
-            Thread.Sleep(500);
+                //Console.WriteLine("Обработка заявки: {0}", id);
+                //Console.WriteLine("{0}",Thread.CurrentThread.Name);
+            Thread.Sleep(Convert.ToInt32(Math.Round(working_time/service_intensity)));
             //}
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < pool.Length; i++)
                 if (pool[i].thread == Thread.CurrentThread)
                     pool[i].in_use = false;
         }
@@ -73,7 +107,9 @@ namespace TPProj{
         public Client(Server server)
         {
             this.server = server;
+#pragma warning disable CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
             this.request += server.proc;
+#pragma warning restore CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
         }
         public void send(int id)
         {
